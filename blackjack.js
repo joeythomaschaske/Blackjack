05c6b2698bbb8ab75fe4170e042aeae32909f316
Joseph Thomaschaske <josephthomaschaske@Josephs-MacBook-Pro.local>
2016-02-18 18:38:49 -0600

    Updated JS

--- blackjack.js
+++ blackjack.js
@@ -1,6 +1,10 @@
 /**
  * Created by josephthomaschaske on 2/18/16.
  */
+var playerScore = 0;
+var computerScore = 0;
+var deck = makeDeck();
+
 function makeDeck()
 {
     var deck = [];
@@ -8,7 +12,7 @@ function makeDeck()
     {
         for(var j = 1; j < 14; ++j)
         {
-            var card = [i, j];
+            var card = [j, i];
             deck.push(card);
         }
     }
@@ -29,10 +33,34 @@ function shuffle(deck)
 
 function createCardDiv(card)
 {
-    var leftX;
-    var rightX;
-    var topY;
-    var bottomY;
-    var div = document.createElement('DIV')
+    var rightX = 949 - (card[0] * 73) + 73;
+    var bottomY = 392 - (card[1] * 98) + 98;
+    var div = document.createElement('DIV');
     div.setAttribute('class', 'card');
+    div.style.backgroundImage = 'url("cards.jpg")';
+    div.style.height = '98px';
+    div.style.width = '73px';
+    div.style.backgroundPosition = rightX + "px " + bottomY + "px";
+    return div;
+}
+
+function drawPlayer()
+{
+    var card = deck.shift();
+    if(card[0] > 1 && card[0] < 10)
+        playerScore += card[0];
+    else if(card[0] >= 10)
+        playerScore += 10;
+    else
+        playerScore += 11;
+    var cards = document.getElementById('player-cards');
+    cards.appendChild(createCardDiv(card));
+    var score = document.getElementById('player-score');
+
+    if(playerScore < 21)
+        score.innerText = playerScore;
+    else if(playerScore == 21)
+        score.innerText = "BLACK JACK!";
+    else
+        score.innerText = "BUST!";
 }
